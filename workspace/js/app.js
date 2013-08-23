396d3362b02d43ec7eaa64e42fd2e23e80272764
Brian Zerangue <brian.zerangue@gmail.com>
2013-08-23 12:42:45 -0500

    initial commit; problems with jQuery Orbit slider; looking at implementing bxslider.com

---
+++ workspace/js/app.js
@@ -0,0 +1,104 @@
+// Foundation JS ---
+
+;(function ($, window, undefined) {
+  'use strict';
+
+  var $doc = $(document),
+      Modernizr = window.Modernizr;
+
+  $(document).ready(function() {
+    $.fn.foundationAlerts           ? $doc.foundationAlerts() : null;
+    $.fn.foundationButtons          ? $doc.foundationButtons() : null;
+    $.fn.foundationAccordion        ? $doc.foundationAccordion() : null;
+    $.fn.foundationNavigation       ? $doc.foundationNavigation() : null;
+    $.fn.foundationTopBar           ? $doc.foundationTopBar() : null;
+    $.fn.foundationCustomForms      ? $doc.foundationCustomForms() : null;
+    $.fn.foundationMediaQueryViewer ? $doc.foundationMediaQueryViewer() : null;
+    $.fn.foundationTabs             ? $doc.foundationTabs({callback : $.foundation.customForms.appendCustomMarkup}) : null;
+    $.fn.foundationTooltips         ? $doc.foundationTooltips() : null;
+    $.fn.foundationMagellan         ? $doc.foundationMagellan() : null;
+    $.fn.foundationClearing         ? $doc.foundationClearing() : null;
+
+    $.fn.placeholder                ? $('input, textarea').placeholder() : null;
+  });
+
+  // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
+  // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
+  // $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'both'});
+  // $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
+  // $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
+
+  // Hide address bar on mobile devices (except if #hash present, so we don't mess up deep linking).
+  if (Modernizr.touch && !window.location.hash) {
+    $(window).load(function () {
+      setTimeout(function () {
+        window.scrollTo(0, 1);
+      }, 0);
+    });
+  }
+
+})(jQuery, this);
+
+
+// Content Orbits
+
+$(window).load(function() {
+     // Welcome Orbit
+     $('#welcomeOrbit').orbit({ animation: 'horizontal-push', fluid: '12x9', advanceSpeed: 8000, animationSpeed: 1000, pauseOnHover:  true, startClockOnMouseOut: true  });
+     $('#featuredOrbit').orbit({ fluid: '12x5', pauseOnHover:  true, startClockOnMouseOut: true });
+ });
+
+
+/**
+* hoverIntent r6 // 2011.02.26 // jQuery 1.5.1+
+* <http://cherne.net/brian/resources/jquery.hoverIntent.html>
+* 
+* @param  f  onMouseOver function || An object with configuration options
+* @param  g  onMouseOut function  || Nothing (use configuration options object)
+* @author    Brian Cherne brian(at)cherne(dot)net
+*/
+(function($){$.fn.hoverIntent=function(f,g){var cfg={sensitivity:7,interval:100,timeout:0};cfg=$.extend(cfg,g?{over:f,out:g}:f);var cX,cY,pX,pY;var track=function(ev){cX=ev.pageX;cY=ev.pageY};var compare=function(ev,ob){ob.hoverIntent_t=clearTimeout(ob.hoverIntent_t);if((Math.abs(pX-cX)+Math.abs(pY-cY))<cfg.sensitivity){$(ob).unbind("mousemove",track);ob.hoverIntent_s=1;return cfg.over.apply(ob,[ev])}else{pX=cX;pY=cY;ob.hoverIntent_t=setTimeout(function(){compare(ev,ob)},cfg.interval)}};var delay=function(ev,ob){ob.hoverIntent_t=clearTimeout(ob.hoverIntent_t);ob.hoverIntent_s=0;return cfg.out.apply(ob,[ev])};var handleHover=function(e){var ev=jQuery.extend({},e);var ob=this;if(ob.hoverIntent_t){ob.hoverIntent_t=clearTimeout(ob.hoverIntent_t)}if(e.type=="mouseenter"){pX=ev.pageX;pY=ev.pageY;$(ob).bind("mousemove",track);if(ob.hoverIntent_s!=1){ob.hoverIntent_t=setTimeout(function(){compare(ev,ob)},cfg.interval)}}else{$(ob).unbind("mousemove",track);if(ob.hoverIntent_s==1){ob.hoverIntent_t=setTimeout(function(){delay(ev,ob)},cfg.timeout)}}};return this.bind('mouseenter',handleHover).bind('mouseleave',handleHover)}})(jQuery);
+
+
+
+
+// Mega Drop JS ---
+(function(a){a(function(){a(".toggleMegaDrop").live("click",function(b){var c=a(b.currentTarget);c.hasClass("open")?(c.removeClass("open"),a("#megaDrop").slideUp(400)):(c.addClass("open"),a("#megaDrop").slideDown(400))})})})(jQuery);
+
+
+ $(document).ready(function() {
+      
+      // Main Nav Mega Dropdowns
+      function addMega(){
+        $(this).addClass("hovering");
+        }
+
+      function removeMega(){
+        $(this).removeClass("hovering");
+        }
+
+    var megaConfig = {
+         interval: 100,
+         sensitivity: 10,
+         over: addMega,
+         timeout: 200,
+         out: removeMega
+    };
+
+    $("li.dropdown").hoverIntent(megaConfig);
+
+      
+    });
+
+
+// Mediaelement.JS
+$('audio,video').mediaelementplayer({
+audioWidth: 170,
+features: ['playpause','duration','volume'],
+// features: ['playpause','progress','current','duration','tracks','volume','fullscreen'],
+success: function(player, node) {
+  $('#' + node.id + '-mode').html('mode: ' + player.pluginType);
+}
+});
+
+
